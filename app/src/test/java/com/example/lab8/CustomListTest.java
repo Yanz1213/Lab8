c89da9319d21f97326cddaea5bd5ecbacf665c33
Yanze Zhong <69833973+Yanz1213@users.noreply.github.com>
2024-03-07 20:17:38 -0700

    22

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,10 +1,13 @@
 package com.example.lab8;
 
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,5 +29,14 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+    @org.junit.Test
+    public void testCountCities() {
+        CustomList cityList = MockCityList();
+        Assert.assertEquals(1, cityList.countCities()); // Should be 1 since mockCityList adds one city
+
+        City anotherCity = new City("Regina", "Saskatchewan");
+        cityList.add(anotherCity);
+        Assert.assertEquals(2, cityList.countCities()); // Should be 2 after adding another city
     }
 }
