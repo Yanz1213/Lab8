3895478617482865902de1b16317ce272a50fbb1
Yanze Zhong <69833973+Yanz1213@users.noreply.github.com>
2024-03-07 20:38:22 -0700

    33

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -38,5 +39,30 @@ public class CustomListTest {
         City anotherCity = new City("Regina", "Saskatchewan");
         cityList.add(anotherCity);
         Assert.assertEquals(2, cityList.countCities()); // Should be 2 after adding another city
+    }
+    @Test
+    public void testHasCity(){
+        list = MockCityList();
+        City city = new City("Charlottetown", "Prince Edward Island");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+        assertFalse(list.hasCity(new City("Paris", "France")));
+    }
+
+    @Test
+    public void testDeleteNonCity(){
+        list = MockCityList();
+        City edm = new City("Edmonton", "AB");
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.delete(edm);
+        });
+    }
+    @Test
+    public void testDeleteCity(){
+        list = MockCityList();
+        City edm = new City("Edmonton", "AB");
+        list.addCity(edm);
+        list.delete(edm);
+        assertFalse(list.hasCity(edm));
     }
 }
